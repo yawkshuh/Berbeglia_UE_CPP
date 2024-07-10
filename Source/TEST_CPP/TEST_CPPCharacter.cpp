1a914221556f5630504505b7411dd13228e8ecb9
Riccardo Berbeglia <yawkshuh@protonmail.com>
2024-07-10 12:44:41 +0000

    Moved camera further away to improve telekinesis aiming

--- Source/TEST_CPP/TEST_CPPCharacter.cpp
+++ Source/TEST_CPP/TEST_CPPCharacter.cpp
@@ -43,7 +43,8 @@ ATEST_CPPCharacter::ATEST_CPPCharacter()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character
+	CameraBoom->TargetOffset.Z = 130.0f;
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
